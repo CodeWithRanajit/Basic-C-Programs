4040173e61c56bff2e7aab3861696d19e18197f4
CodeWithRanajit <ranajitchattaraj2004@gmail.com>
2025-05-14 10:10:19 +0530

    First C++ Program

---
+++ Basic_C++_1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>   //Header File 
+using namespace std;   // Namespace std 
+int main()   // main Function and Entry point of the code
+{
+  cout<<"Hello World!";     // code part
+  return 0;      // return 
+}
